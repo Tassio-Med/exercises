919a1f0551a518cbd013007205af6800c7be239e
Tassio Med <tassiohfmed@gmail.com>
2022-04-15 11:54:30 -0300

    exercício 'find the stray number' 15.04.22

---
+++ estudo-pessoal/codewars/april03.1.js
@@ -0,0 +1,37 @@
+/*
+FIND THE STRAY NUMBER
+
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+
+*/
+
+function stray(numbers) {
+  let max = Math.max(...numbers);
+  let min = Math.min(...numbers);
+  return numbers.filter(i => i === max).length === 1 ? max : min;
+}
+stray([1, 1, 1, 1, 2]);  // 2
+stray([4, 4, 8, 4, 4]);  // 8
+
+//OUTRAS SOLUÇÕES
+
+//1
+function stray(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
+      return numbers[i];
+    }
+  }
+}
+stray([1, 1, 1, 1, 2]);  // 2
+stray([4, 4, 8, 4, 4]);  // 8
+
+
