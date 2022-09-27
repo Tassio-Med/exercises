bc1d7b2e84d7540f30c233efcd0d38384eb429ba
Tassio Med <tassiohfmed@gmail.com>
2022-09-27 12:16:12 -0300

    feat: requisição do array teams criada

--- trybe/node.js/introducao/dia2/soccer-team-manager/src/app.js
+++ trybe/node.js/introducao/dia2/soccer-team-manager/src/app.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+const OK = 200;
+
+const teams = [
+  {
+    id: 1,
+    name: 'Clube de Regatas Flamengo',
+    initials: 'CRF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.get('/', (req, res) => res.status(OK).json({ message: 'Olá Mundo!' }));
+
+app.get('/', (req, res) => res.status(OK).json({ teams }));
 
 module.exports = app;
