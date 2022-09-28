4987f0b58ef24df0ecf6e1152213cafc81e4fb18
Tassio Med <tassiohfmed@gmail.com>
2022-09-27 21:32:03 -0300

    feat: criado endpoint do tipo GET com a rota /myActivities/:id, que possa listar uma atividade do array por id

--- trybe/node.js/introducao/dia2/exercicio-pratico/src/app.js
+++ trybe/node.js/introducao/dia2/exercicio-pratico/src/app.js
@@ -2,4 +2,27 @@ const express = require('express');
 
 const app = express();
 
+const activities = [
+  {
+    id: 1,
+    description: 'Banho no cachorro',
+    status: 'A fazer',
+  },
+  {
+    id: 2,
+    description: 'Cortar a grama',
+    status: 'A fazer',
+  },
+  {
+    id: 3,
+    description: 'Estudar JavaScript',
+    status: 'Feito',
+  },
+];
+
+app.get('/myActivities/:id', (req, res) => {
+  const activity = activities.find(({ id }) => id === Number(req.params.id));
+  res.status(201).json(activity);
+});
+
 module.exports = app;
