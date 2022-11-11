9afa631e3fd8796234070d2ec5590211ff1406d3
Tassio-Med <tassiohfmed@gmail.como>
2022-11-10 21:21:46 -0300

    :sparkles:feat: pegando todos os itens e listando-os

--- trybe/firebase/react-firebase/src/App.js
+++ trybe/firebase/react-firebase/src/App.js
@@ -1,75 +1,117 @@
 import { useState } from 'react'
-import './app.css'
-
 import { db } from './firebaseConnection';
-import { doc, /*setDoc,*/ collection, addDoc, getDoc } from 'firebase/firestore';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore'
+
+import './app.css';
 
 function App() {
-  const [título, setTítulo] = useState('');
+  const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
+
+  const [posts, setPosts] = useState([]);
+
 
   async function handleAdd(){
     // await setDoc(doc(db, "posts", "12345"), {
-    //   titulo: título,
+    //   titulo: titulo,
     //   autor: autor,
     // })
     // .then(() => {
-    //   console.log("DADOS REGISTRADOS NO BANCO!")
+    //   console.log("DADOS REGISTRADO NO BANCO!")
     // })
     // .catch((error) => {
     //   console.log("GEROU ERRO" + error)
-    // })
+    // }) 
+
+
     await addDoc(collection(db, "posts"), {
-      titulo: título,
+      titulo: titulo,
       autor: autor,
     })
     .then(() => {
-      console.log("DADOS REGISTRADOS NO BANCO!")
+      console.log("CADASTRADO COM SUCESSO")
       setAutor('');
-      setTítulo('');
+      setTitulo('')
     })
     .catch((error) => {
-      console.log("GEROU ERRO" + error)
+      console.log("ERRO " + error)
     })
+
+
   }
 
+
   async function buscarPost(){
+    // const postRef = doc(db, "posts", "vFvZAyFqebXFsFv0X89l")
+    // await getDoc(postRef)
+    // .then((snapshot) => {
+    //   setAutor(snapshot.data().autor)
+    //   setTitulo(snapshot.data().titulo)
 
-    const postRef = doc(db, "posts", "1234")
+    // })
+    // .catch(()=>{
+    //   console.log("ERRO AO BUSCAR")
+    // })
 
-    await getDoc(postRef)
+    const postsRef = collection(db, "posts")
+    await getDocs(postsRef)
     .then((snapshot) => {
-      setAutor(snapshot.data().autor)
-      setTítulo(snapshot.data().título)
+      let lista = [];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        })
+      })
+
+      setPosts(lista);
+
     })
-    .catch(() => {
-      console.log("ERRO AO BUSCAR")
+    .catch((error) => {
+      console.log("DEU ALGUM ERRO AO BUSCAR")
     })
+
+
   }
 
   return (
     <div>
-      <h1>React JS + Firebase</h1>
-      <div className="container">
-        <label htmlFor="">Título</label>
-        <textarea
-          type="text"
-          placeholder="Digite o título"
-          value={título}
-          onChange={ (e) => setTítulo(e.target.value) }
-          />
+      <h1>ReactJS + Firebase :)</h1>
 
-        <label htmlFor="">Autor:</label>
-        <input
-          type="text"
-          placeholder="Autor do post"
-          value={autor}
-          onChange={ (e) => setAutor(e.target.value) }
-        />
+    <div className="container">
+      <label>Titulo:</label>
+      <textarea 
+        type="text"
+        placeholder='Digite o titulo'
+        value={titulo}
+        onChange={ (e) => setTitulo(e.target.value) }
+      />
 
-        <button onClick={handleAdd}>Cadastrar</button>
-        <button onClick={buscarPost}>Buscar Post</button>
-      </div>
+      <label>Autor:</label>
+      <input 
+        type="text" 
+        placeholder="Autor do post"
+        value={autor}
+        onChange={(e) => setAutor(e.target.value) }
+      />
+
+      <button onClick={handleAdd}>Cadastrar</button>
+      <button onClick={buscarPost}>Buscar post</button>
+
+      <ul>
+        {posts.map( (post) => {
+          return(
+            <li key={post.id}>
+              <span>Titulo: {post.titulo} </span> <br/>
+              <span>Autor: {post.autor}</span> <br/>  <br/>
+            </li>
+          )
+        })}
+      </ul>
+
+    </div>
 
     </div>
   );
