11faa157ab7aaad99e85c7a4368590b0f7d37360
Tassio-Med <tassiohfmed@gmail.como>
2023-02-10 20:46:56 -0300

    :sparkles: feat: primeiro exercício de lógica de programção em Javascript

---
+++ javascript/challenge01.js
@@ -0,0 +1,17 @@
+// EXERCÍCIO 1
+// Faça as 4 operações básica com base nos números 2 e 4:
+// Ao final de cada operação imprima na tela no seguinte formato: 
+// Soma: 2 + 4 = x
+// Subtração : 2 - 4 = x
+// Divisão: 2 / 4 = x
+// Multiplicação: 2 * 4 = x
+// ---------------------------------------------------------------
+
+let num1 = 2;
+let num2 = 4;
+
+console.log('A divisão é igual a ' + (num1 / num2));
+console.log('A subtração é igual a ' + (num1 - num2));
+console.log('A soma é igual a ' + (num1 + num2));
+console.log('A multiplicação é igual a ' + (num1 * num2));
+
