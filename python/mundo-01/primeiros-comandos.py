8417b848b2f1e9c7feb83f7b2dead16aac5c71f5
Tassio-Med <tassiohfmed@gmail.como>
2023-04-26 12:07:35 -0300

    :sparkles: feat: organizando para iniciar os estudos em python

---
+++ python/mundo-01/primeiros-comandos.py
@@ -0,0 +1,12 @@
+nome="Humberto Delgado"
+empresa='FIAP'
+qtde_funcionarios=500
+mediaMensalidade=856.50
+
+nome="Humberto Delgado"
+empresa='FIAP'
+qtde_funcionarios=500
+mediaMensalidade=856.50
+print(nome + " trabalha na empresa " + empresa)
+print("Possui: ", qtde_funcionarios, " funcionarios.")
+print("A média da mensalidade é de: " + str(mediaMensalidade))
