c802c00fe2c7e4fea00047cd860fce7a9512e60e
Tassio-Med <tassiohfmed@gmail.como>
2023-04-27 20:52:57 -0300

    :sparkles: feat: tipo boolean e type annotation

---
+++ typescript/booleanType.ts
@@ -0,0 +1,10 @@
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+let concluido: boolean = false;
+
+if(!concluido){
+  console.log('Terefa concluída com sucesso!')
+} else {
+  console.log('Terefa pendente!')
+}
---
+++ typescript/typeAnnotation.ts
@@ -0,0 +1,13 @@
+let carro: {
+  nome: string,
+  ano: number,
+  preco: number
+};
+
+carro = {
+  nome: 'ford',
+  ano: 2019,
+  preco: 27.14
+}
+
+console.log(carro)
