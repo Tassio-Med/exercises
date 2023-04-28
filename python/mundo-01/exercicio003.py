16449367e6c828f1570bc3436682ae8dc79f07d9
Tassio-Med <tassiohfmed@gmail.como>
2023-04-28 12:06:09 -0300

    :sparkles: feat: exercicios 003 e 004 adicionados.

---
+++ python/mundo-01/exercicio003.py
@@ -0,0 +1,4 @@
+number1 = int(input('Digite um valor: '))
+number2 = int(input('Digite outro valor: '))
+s = number1 + number2
+print('A soma entre {} e {} é igual a {}!'.format(number1, number2, s))
