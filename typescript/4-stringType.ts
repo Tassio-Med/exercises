114bd5091d1276e58e7b700b98d5f5a561fb9dc3
Tassio-Med <tassiohfmed@gmail.como>
2023-05-03 21:15:14 -0300

    :sparkles: feat: atipo bigint e number adicionados e arquivos organizados por renomeação

---
+++ typescript/4-stringType.ts
@@ -0,0 +1,8 @@
+let meuNome: string = 'Tássio Medeiros';
+console.log(meuNome);
+
+let nomeDela: string = 'Amanda Portela';
+console.log(nomeDela);
+
+let fraseMassa: string = `${meuNome} e ${nomeDela} estão namorando a quase cinco anos.`;
+console.log(fraseMassa);
