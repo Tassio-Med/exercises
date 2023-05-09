3da1ef2f3ef6e8ac4c141cd1199e2bb03bcfce1a
Tassio-Med <tassiohfmed@gmail.como>
2023-05-09 11:52:15 -0300

    exercicios 009 e 010 colocados em prática

---
+++ python/mundo-01/exercicio009.py
@@ -0,0 +1,15 @@
+number = int(input('Digite um número para ver a sua tabuada: '))
+n1 = number * 1
+n2 = number * 2
+n3 = number * 3
+n4 = number * 4
+n5 = number * 5
+n6 = number * 6
+n7 = number * 7
+n8 = number * 8
+n9 = number * 9
+n10 = number * 10
+print('-' * 11)
+print('{} x 1 = {} \n{} x 2 = {} \n{} x 3 = {} \n{} x 4 = {} \n{} x 5 = {} \n{} x 6 = {} \n{} x 7 = {} \n{} x 8 = {} \n{} x 9 = {} \n{} x 10 = {}'
+      .format(number, n1, number, n2, number, n3, number, n4, number, n5, number, n6, number, n7,number, n8, number, n9, number, n10))
+print('-' * 11)
---
+++ python/mundo-01/exercicio010.py
@@ -0,0 +1,3 @@
+real = float(input('Quanto dinheiro você tem na carteira? R$'))
+dolar = real / 5
+print('Com R${}, você pode comprar US${}'.format(real, dolar))
