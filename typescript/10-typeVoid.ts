5c9a86130b1aa93189908d53d044b45055c6a460
Tassio-Med <tassiohfmed@gmail.como>
2023-05-17 12:36:53 -0300

    feat: type Void colocado em prática

---
+++ typescript/10-typeVoid.ts
@@ -0,0 +1,18 @@
+// Exemplo 01 - Funções
+
+
+function logError(errorMessage: string) {
+  console.log(errorMessage);
+}
+
+logError('Required data');
+
+// Exemplo 02 - Funções
+
+const logError2 = (errorMessage2: string) => {
+  console.log(errorMessage2);
+}
+
+logError2('Required data again');
+
+
