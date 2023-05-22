520df0bc11caf94fa151bea63b14da61266cf481
Tassio-Med <tassiohfmed@gmail.como>
2023-05-22 19:52:17 -0300

    feat: tipos null e undefined colocados em prática

---
+++ typescript/11-nulUndefinedType.ts
@@ -0,0 +1,17 @@
+// Exemplo 01: null;
+
+let variavelTesteNull = null;
+console.log(variavelTesteNull);
+console.log(typeof variavelTesteNull);
+
+// Exemplo 02: undefined;
+
+let variavelTesteUndefined;
+console.log(variavelTesteUndefined);
+console.log(typeof variavelTesteUndefined);
+
+// Diferenças e similaridades: Null vs Undefined
+console.log('Exemplo 01:', null == undefined); /* true */
+console.log('Exemplo 02:', null === undefined); /* false */
+
+
