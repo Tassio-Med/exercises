9ca902610c6bbd7e8f975016d016278e7a2e58af
Tassio-Med <tassiohfmed@gmail.como>
2023-05-25 15:11:52 -0300

    feat: estudando as diretivas

--- vue/1-vue-cli/src/components/Info.vue
+++ vue/1-vue-cli/src/components/Info.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h3>Estou trabalhandono momento</h3>
+    <h3 v-if="esta_trabalhando">Estou trabalhando momento</h3>
+    <h3 v-else>Estou em busca de novas oportunidades!</h3>
     <p>Utilizo as seguintes tecnologias:</p>
     <ul>
       <li>Javascript</li>
@@ -8,11 +9,19 @@
       <li>Python</li>
       <li>Typescript</li>
     </ul>
+    <p v-show='mostrar_email'>Mande uma mensagem para: {{ email }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Info',
+  data() {
+    return {
+      esta_trabalhando: true,
+      email: 'tassiohm@gmail.com',
+      mostrar_email: true
+    }
+  }
 }
 </script>
