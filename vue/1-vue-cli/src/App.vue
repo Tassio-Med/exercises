c8c7e0ecf18833d81c5cfd26434cc4d2a05237d3
Tassio-Med <tassiohfmed@gmail.como>
2023-05-25 11:34:59 -0300

    feat: estudando LifeCycle Hooks

--- vue/1-vue-cli/src/App.vue
+++ vue/1-vue-cli/src/App.vue
@@ -2,16 +2,20 @@
   <div>
    <PrimeiroComponente />
     <h2>Agora eu estou testando os {{ dados }} em componentes.</h2>
+    <LifeCycle />
+
   </div>
 </template>
 
 <script>
    import PrimeiroComponente from './components/PrimeiroComponente.vue'
+   import LifeCycle from './components/LifeCycle.vue'
   
   export default {
     name:'App',
     components: {
-      PrimeiroComponente
+      PrimeiroComponente,
+      LifeCycle
     }, 
     data(){
       return {
---
+++ vue/1-vue-cli/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'LifeCycle',
+  data(){
+    return {
+      nome: 'Ainda não sei meu nome'
+    }
+  },
+  created(){
+    setTimeout(() => {
+      this.nome = 'Mateus'
+    }, 1000);
+  },
+  mounted(){
+    setTimeout(() => {
+      this.nome = 'Tássio'
+    }, 4000);
+  }
+}
+</script>
