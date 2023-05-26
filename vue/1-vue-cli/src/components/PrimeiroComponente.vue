be845e00f0bf34ed1022517b2537dcce17752c25
Tassio-Med <tassiohfmed@gmail.como>
2023-05-26 16:22:16 -0300

    feat: múltiplos eventos colocados em prática

--- vue/1-vue-cli/src/components/PrimeiroComponente.vue
+++ vue/1-vue-cli/src/components/PrimeiroComponente.vue
@@ -2,13 +2,19 @@
   <div>
     <h1>Olá Vue!</h1>
     <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+    <MultiplosEventos />
   </div>
 </template>
 
 <script>
  
+  import MultiplosEventos from './MultiplosEventos.vue'
+
   export default {
     name:'PrimeiroComponente',
+    components: {
+      MultiplosEventos,
+    },
     data() {
       return {
         nome: 'Tássio',
