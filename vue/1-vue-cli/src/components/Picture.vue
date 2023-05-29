9ba73ed7663eaf27f7bfbbef4e3f8e2a4c243230
Tassio-Med <tassiohfmed@gmail.como>
2023-05-29 11:54:34 -0300

    feat: estudando método emit em Vue

--- vue/1-vue-cli/src/components/Picture.vue
+++ vue/1-vue-cli/src/components/Picture.vue
@@ -1,22 +1,30 @@
 <template>
   <div>
     <img :src="avatar" :alt="descricao">
+    <MudarImagem @mudar-imagem="trocarImagem" />
     <Reutilizacao />
   </div>
 </template>
 <script>
 
 import Reutilizacao from './Reutilizacao.vue'
+import MudarImagem from './MudarImagem.vue'
 
 export default {
   name:'Picture',
   components: {
     Reutilizacao,
+    MudarImagem,
   },
   data(){
     return {
       avatar: '/img/avatar.png',
       descricao: 'Imagem de avatar'
+    }
+  },
+  methods: {
+    trocarImagem() {
+      this.avatar = '/img/avatar2.png'
     }
   }
 
