66ae3d76f42e59b292e0a3b0e18f872b5f32d81b
Tassio-Med <tassiohfmed@gmail.como>
2023-06-01 19:55:02 -0300

    feat: exercício de função com tipagem adicionado

--- typescript/modulo-03/14-functions.ts
+++ typescript/modulo-03/14-functions.ts
@@ -1,15 +1,30 @@
+export {};
+
 // Exemplo 01 - Functions (Named function)
 
+function somarNumeros(num1: number, num2: number): number {
+  return num1 + num2;
+}
 
+const resultado = somarNumeros(2, 2);
+console.log(resultado);
 
 // Exemplo 02 - Função Anônima (Function Expression)
 
+const saudar = function (mensagem: string) {
+  return mensagem;
+};
 
+console.log(saudar('Olá mundo!'));
 
 // Exemplo 03 - (Arrow Function Expression)
 
+const saudar02 = (mensagem: string) => mensagem;
+console.log(saudar02('IIHUUU!'));
 
 
 // Exemplo 01 - (Function constructor)
 
+const saudar03 = new Function('mensagem', 'return "Fala " + mensagem');
+console.log(saudar03('Pessoal!'));
 
