a8b33d02e2551f7509ee651d007e26e641c8085d
Tassio-Med <tassiohfmed@gmail.como>
2023-06-29 16:08:11 -0300

    feat: mapped type colocado em prática

---
+++ typescript/modulo-06/32-mappedTypes.ts
@@ -0,0 +1,62 @@
+export {};
+
+// ==> Exemplo 01 - Mapped Types
+
+type Usuario = {
+  nome: string;
+  endereco: string;
+  telefone: string;
+  idade: number;
+};
+
+type UsuarioOpcional = {
+  nome?: string;
+  endereco?: string;
+  telefone?: string;
+  idade?: number;
+};
+
+type UsusarioSomenteLeitura = {
+  readonly nome: string;
+  readonly endereco: string;
+  readonly telefone: string;
+  readonly idade: number;
+};
+
+// { [P in K]: T }
+
+type UsuarioMappedType = {
+  [P in keyof Usuario]?: Usuario[P]; // for in
+};
+
+// 'nome' | 'endereco' | 'telefone' | 'idade'
+
+const usuarioMapped: UsuarioMappedType = {
+  nome: 'Tássio Medeiros',
+  endereco: 'Rua Lsldmsld, 34',
+}
+
+console.log(usuarioMapped);
+
+
+// ==> Exemplo 02 - Outros Tipos Avançados
+
+interface Livro {
+  titulo: string;
+  autor: string | null;
+  preco: number;
+  numeroPaginas: number;
+}
+
+type Artigo = Omit<Livro, 'numeroPaginas'>;
+
+type LivroModelo = Readonly<Livro>;
+
+const livro: LivroModelo = {
+  autor: 'Boris Cherny',
+  numeroPaginas: 324,
+  preco: 19.99,
+  titulo: 'Programming Typescript: Making Your JavaScript Applications Scale'
+}
+
+// livro.autor = "Tássio Medeiros"
