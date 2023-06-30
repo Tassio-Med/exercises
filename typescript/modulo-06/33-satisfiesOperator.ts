fdf02d2d74389d68194007d1f5b40952f16771b0
Tassio-Med <tassiohfmed@gmail.como>
2023-06-30 11:28:34 -0300

    feat: satisfies operator colocado em prática

---
+++ typescript/modulo-06/33-satisfiesOperator.ts
@@ -0,0 +1,55 @@
+export { };
+
+// ==> Exemplo 01: Satisfies Operato
+
+type Cidade = NomeCidade | CidadeCoordenadas;
+
+type NomeCidade = "São Paulo" | "Rio de Janeiro" | "Salvador" | "Belo Horizonte";
+
+type CidadeCoordenadas = {
+  x: number;
+  y: number;
+};
+
+type Pessoa = {
+  localNascimento: Cidade;
+  residenciaAtual: Cidade;
+};
+
+const pessoa = {
+  localNascimento: "Rio de Janeiro",
+  residenciaAtual: { x: 10, y: 20 },
+} satisfies Pessoa
+
+console.log(pessoa.localNascimento.toUpperCase());
+
+// ==> Exemplo 02: Satisfies Operator
+
+type Connection = {};
+
+declare function createConnection(
+  host: string,
+  port: string,
+  reconnect: boolean,
+  poolsize: number
+): Connection;
+
+type Configuration = {
+  host: string;
+  port: string | number;
+  tryReconnect: boolean | (() => boolean);
+  poolSize?: number;
+};
+
+const config = {
+  host: "localhost",
+  port: 3306,
+  tryReconnect: () => true,
+  poolSize: 10,
+} satisfies Configuration;
+
+function connect() {
+  let { host, port, tryReconnect } = config;
+
+  createConnection(host, `${port}`, tryReconnect(), 10);
+}
