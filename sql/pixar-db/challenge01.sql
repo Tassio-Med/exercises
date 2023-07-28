c94c240dc3a7895f93a16fa7fcad1e4e970367a6
Tassio-Med <tassiohfmed@gmail.como>
2023-07-28 13:05:17 -0300

    feat: colocado em prática o primeiro desafio

---
+++ sql/pixar-db/challenge01.sql
@@ -0,0 +1,7 @@
+SELECT * FROM pixar.movies;
+INSERT INTO pixar.movies (title, director, `year`, length_minutes) VALUES
+('Monstros SA', 'Pete Docter', 2001, 92),
+('Procurando Nemo', 'John Lasseter', 2003, 107),
+('Os Incríveis', 'Brad Bird', 2004, 116),
+('WALL-E', 'Pete Docter', 2008, 104);
+
