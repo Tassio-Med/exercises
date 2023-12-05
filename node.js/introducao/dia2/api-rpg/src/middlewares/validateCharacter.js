6767e985dd51c430c172cf947776963a9adb6393
Tassio-Med <tassiohfmed@gmail.como>
2023-12-05 16:36:42 -0300

    feat: adicionados middleware de validação de character e middleware de validação de token

---
+++ node.js/introducao/dia2/api-rpg/src/middlewares/validateCharacter.js
@@ -0,0 +1,10 @@
+const validateCharacter = (req, res, next) => {
+  const requiredProperties = ['nome', 'sigla'];
+  if (requiredProperties.every((property) => property in req.body)) {
+    next(); 
+  } else {
+    res.sendStatus(400); 
+  }
+};
+
+module.exports = validateCharacter;
