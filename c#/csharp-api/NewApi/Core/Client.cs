36dc89a90ee1291507b3f846e1b0d224bc4e4818
Tassio-Med <tassiohfmed@gmail.como>
2024-04-12 19:22:17 -0300

    feat: arquivos de métodos criados

---
+++ c#/csharp-api/NewApi/Core/Client.cs
@@ -0,0 +1,10 @@
+namespace NewApi.Core;
+
+public class Client
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal AccountBalance { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ c#/csharp-api/NewApi/Core/ClientRequest.cs
@@ -0,0 +1,27 @@
+namespace NewApi.Core;
+
+public class ClientRequest
+{
+    public string? Name { get; set; }
+    public decimal AccountBalance { get; set; }
+
+    public Client CreateClient(int id)
+    {
+        return new Client
+        {
+            Id = id,
+            Name = Name,
+            AccountBalance = AccountBalance,
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now
+        };
+    }
+
+    public Client UpdateClient(Client client)
+    {
+        client.Name = Name;
+        client.AccountBalance = AccountBalance;
+        client.UpdatedAt = DateTime.Now;
+        return client;
+    }
+}
