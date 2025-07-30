1b1aa0f5629e43ea0d58275aca4afad1534039f6
Tassio Medeiros <tassiohfmed@gmail.com>
2025-07-30 19:53:17 -0300

    feat: iniciando estudos de Dart

---
+++ dart/primeiro_teste.dart
@@ -0,0 +1,8 @@
+void main() {
+  var nome = "João"; // Inferência: String
+  int idade = 25;    // Tipo explícito
+  double altura = 1.75;
+  bool isEstudante = true;
+
+  print("Nome: $nome, Idade: $idade");
+}
